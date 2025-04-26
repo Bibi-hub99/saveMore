f2c3d2f768842150cab13a495e8abd968050a114
Bibi-hub99 <anxumalo000@gmail.com>
2025-04-26 20:17:14 +0200

    validations done

--- frontend-app/src/components/simpleButton.jsx
+++ frontend-app/src/components/simpleButton.jsx
@@ -5,7 +5,13 @@ function SimpleBtn(props){
     }
 
     return (
-        <button type={props.btnType} name={props.btnName} id={props.btnId} className={`${props.btnStyle}`} style={btnStyle}>{props.innerText}</button>
+        <button type={props.btnType}
+         name={props.btnName} 
+         id={props.btnId} 
+         className={`${props.btnStyle}`} 
+         style={btnStyle}
+         onClick={props.handleClick}
+         >{props.innerText}</button>
     )
 
 }
